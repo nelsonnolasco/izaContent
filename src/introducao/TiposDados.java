97c8b484bab14f02e739f3cc488f37645bc4a893
Nelson de Campos Nolasco <nelsonnolasco@gmail.com>
2024-07-16 19:50:00 -0300

    Package introducao

---
+++ src/introducao/TiposDados.java
@@ -0,0 +1,47 @@
+package introducao;
+
+public class TiposDados {
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com 0, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com 0, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // Mesmo sabendo que numeroNormal é igual a numeroCurto,
+        // não é possível atribuir a numeroCurto2
+
+
+        /*
+         * esta linha é considerada como declaração de variável iniciamos a existência
+         * variável numero com valor 5 regra: tipo + nome + valor
+         */
+        int numero = 5;
+
+        /*
+         * na linha abaixo iremos alterar o valor do variável para 10 observe que o tipo
+         * não é mais necessário, pois a variável já foi declarada anteriormente
+         */
+        numero = 10;
+
+        System.out.print(numero);
+
+        /*
+         * ao usar a palavra reservada final, você determina que jamais
+         * esta variável poderá obter outro valor;
+         * logo a linha 25 vai apresentar um erro de compilação
+         * isso é considerado uma CONSTANTE na linguagem Java
+         */
+        final double VALOR_DE_PI = 3.14;
+
+        VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação!
+
+    }
+}
